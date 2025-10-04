4762a4fb001011ac8c0bd72eaadff19c3655e14c
Julia Nunes Pedrosa <101535045+julianunesp@users.noreply.github.com>
2025-10-04 19:08:13 -0300

    Fix init.py file for input package

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,8 @@
 """Services module for Study Schedule Generator."""
 from .parser_service import parse_udemy_list, parse_spreadsheet
 from .scheduler_service import apply_multiplier, schedule_classes, create_calendar_events
+from .html_parser_agent import get_html_parser
+from .google_service import get_google_service
 
 __all__ = [
     'parse_udemy_list',
@@ -8,5 +10,7 @@ __all__ = [
     'apply_multiplier',
     'schedule_classes',
     'create_calendar_events',
+    'get_html_parser',
+    'get_google_service',
 ]
 
